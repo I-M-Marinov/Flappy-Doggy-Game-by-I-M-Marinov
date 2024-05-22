ecc4f749625c9aebdbfb5ae468e731f16489f14e
I-M-Marinov <lcfrrr@gmail.com>
2024-05-22 23:40:44 +0300

    Added an icon to the application Fixed the points text, so now when you start the the application it does now show anything and when you start it  shows 0

--- Floppy-Game-by-I-M-Marinov/Flappy Doggie.Designer.cs
+++ Floppy-Game-by-I-M-Marinov/Flappy Doggie.Designer.cs
@@ -111,9 +111,8 @@
             scoreText.Font = new Font("Little Dinosaur", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
             scoreText.Location = new Point(151, 15);
             scoreText.Name = "scoreText";
-            scoreText.Size = new Size(39, 36);
+            scoreText.Size = new Size(0, 36);
             scoreText.TabIndex = 6;
-            scoreText.Text = "0";
             // 
             // obstacleBottom2
             // 
